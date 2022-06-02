dbbc26c720add297860d701799d4cb5a27cd1ada
cocchialorenzo9 <10623885@polimi.it>
2022-06-02 17:15:53 +0200

    Required components created

---
+++ src/components/GJNumberLabel.vue
@@ -0,0 +1,35 @@
+<template>
+<!-- component that layouts the provided number and displays the description for each of those -->
+<div class="gj-number-label">
+  <div class="gj-number-label__number">
+    <slot name="number">
+      <span class="gj-number-label__number-value">{{ number }}</span>
+    </slot>
+  </div>
+  <div class="gj-number-label__description">
+    <slot name="description">
+      <span class="gj-number-label__description-value">{{ description }}</span>
+    </slot>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "GJNumberLabel",
+  props: {
+      number: {
+        type: Number,
+        required: true
+      },
+      description: {
+        type: String,
+        required: true
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/GJNumbersView.vue
@@ -0,0 +1,40 @@
+<template>
+<!-- component that takes the title and a list of number-description couples and
+shows them through GJNumbersView-->
+<div class="gj-numbers-view">
+  <h1>{{ title }}</h1>
+  <div class="gj-numbers-view__list">
+    <!-- for each element in GJList, create a GJNumbersView component -->
+    <GJNumberLabel v-for="(number, description) in list"
+                   :key="number"
+                   :number="number"
+                   :description="description">
+    </GJNumberLabel>
+  </div>
+</div>
+</template>
+
+<script>
+import GJNumberLabel from './GJNumberLabel.vue'
+
+export default {
+  name: "GJNumbersView",
+  components: {
+    GJNumberLabel
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    GJList: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
